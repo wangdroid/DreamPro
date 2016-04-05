ab4281194f8d7f245c4a0d019378d853d47db727
xika <709147468@qq.com>
2016-04-05 17:33:57 +0800

    add Teacher.java file

---
+++ Teacher.java
@@ -0,0 +1,33 @@
+public class Teacher {
+    //姓名
+    private String name;
+    //年龄
+    private int age;
+    //性别
+    private String sex;
+    
+    
+    private void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    private void setAge(int age) {
+        this.age = age;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    private void setSex(String sex) {
+        this.sex = sex;
+    }
+    
+    public String getSex() {
+        return sex;
+    }
+}
